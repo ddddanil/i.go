50506f7e1c05570faef852304b9f0fca5007ea7b
Danil Doroshin <ddddanil5555@gmail.com>
2021-11-18 19:12:22 +0300

    Implement register API

--- api/api.go
+++ api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"github.com/ddddanil/i.go/shortener"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
+	"time"
 )
 
 type api struct {
@@ -14,6 +17,26 @@ func RegisterApi(router *gin.RouterGroup, tx *gorm.DB) {
 	router.POST("/register", api.registerUrl)
 }
 
-func (api *api) registerUrl(g *gin.Context) {
+type urlView struct {
+	url      string `binding:"required"`
+	expireIn int
+}
 
+func (api *api) registerUrl(c *gin.Context) {
+	var url urlView
+	if err := c.ShouldBind(&url); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var short shortener.ShortUrl
+	if url.expireIn != 0 {
+		short = shortener.NewShortUrl(url.url,
+			shortener.WithExpiration(time.Duration(url.expireIn)*time.Minute))
+	} else {
+		short = shortener.NewShortUrl(url.url)
+	}
+	api.tx.Create(&short)
+	c.JSON(http.StatusOK, gin.H{
+		"short": short.Shortened,
+	})
 }
